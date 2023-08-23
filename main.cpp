0f12d30913b58736d231b35017abb57822c937fc
Novak <novaldix@gmail.com>
2023-08-23 06:46:33 +0300

    24_5_3 added solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
+#include <windows.h>
 #include <iostream>
+#include <ctime>   // для работы со временем
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void timer1(int delay) {
+    cout << "Отсчет пошёл:" << endl;
+    while(delay) {
+        Sleep(1000);
+        --delay;
+        time_t now = time(nullptr);
+        std::tm* local = localtime(&now);
+        cout << "Осталось " << delay << " секунд ";
+        cout << "(" << std::put_time(local, "%M:%S") << ")" << endl;
+    }
+
+    cout << "DING! DING! DING!" << endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    int minutes{0};
+    int seconds{0};
+    cout << "Введите количество минут (либо 0): ";
+    std::cin >> minutes;
+    cout << "Введите количество секунд (либо 0): ";
+    std::cin >> seconds;
+
+    timer1(minutes * 60 + seconds);
+
     return 0;
 }
